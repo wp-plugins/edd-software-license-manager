fb4a8e8f0b7391099ead94cb6bf266f5274f2ab5
flowdee <flowdee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-17 08:00:11 +0000

    Preparing for 1.0.0 release
    git-svn-id: https://plugins.svn.wordpress.org/edd-software-license-manager/trunk@1200811 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,94 @@
+=== EDD Software License Manager ===
+Contributors: flowdee
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BCQRWXARSHQTW
+Tags: edd, edd license, edd software license, software license, software license manager, easy digital downloads, edd licensing
+Requires at least: 3.5.1
+Tested up to: 4.2.2
+Stable tag: 1.0.0
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Seamless integration between Easy Digital Downloads and Software License Manager.
+
+== Description ==
+Seamless integration between Easy Digital Downloads and Software License Manager.
+
+= Features =
+
+* Automatically creates license keys for each sale with Easy Digital Downloads
+* Licensing is optional and can be activated/deactivated individually
+* Normal and variable pricing supported
+* Send generated license keys to your customers within your existing email notifications
+* Visit the [plugin page](http://coder.flowdee.de/edd-software-license-manager/)
+* Regular updates and improvements: Go though the [changelog](https://wordpress.org/plugins/edd-software-license-manager/changelog/)
+
+> <strong>Please note</strong><br>
+> The license validation part for your distributed plugins and themes is not part of this plugin. Therefore please take a look into the Software License Manager documentation.
+
+= Support =
+
+* Browse [issue tracker](https://github.com/flowdee/edd-software-license-manager/issues) on GitHub
+* [Follow me on Twitter](https://twitter.com/flowdee) to stay in contact and informed
+
+= Credits =
+
+* [Easy Digital Downloads](https://wordpress.org/plugins/easy-digital-downloads/)
+* [Software License Manager](https://wordpress.org/plugins/software-license-manager/)
+
+== Installation ==
+
+The installation and configuration of the plugin is as simple as it can be.
+
+= Using The WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Search for 'edd software license manager'
+3. Click 'Install Now'
+4. Activate the plugin on the Plugin dashboard
+
+= Uploading in WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Navigate to the 'Upload' area
+3. Select `edd-software-license-manager.zip` from your computer
+4. Click 'Install Now'
+5. Activate the plugin in the Plugin dashboard
+
+= Using FTP =
+
+1. Download `edd-software-license-manager.zip`
+2. Extract the `edd-software-license-manager` directory to your computer
+3. Upload the `edd-software-license-manager` directory to the `/wp-content/plugins/` directory
+4. Activate the plugin in the Plugin dashboard
+
+== Configuration ==
+The plugin is really simple and well structured so you don’t have to prepare a lot in order to get it working.
+
+1. After the successful installation you will find a prepared options page here: “Downloads” > “Settings” > “Extensions”
+2. Enter your Software License Manager API credentials
+3. Go to your Downloads and activate licensing where it's required
+
+== Frequently Asked Questions ==
+
+= Is it necessary to install both plugins on the same WordPress installation? =
+No! That's one of the biggest benefits of this integration. Easy Digital Downloads and the Software License Manager can be installed on different sites.
+
+= Can I use this plugin to validate the generated license keys? =
+No! The license validation part for your distributed plugins and themes is not part of this plugin. Therefore please take a look into the Software License Manager documentation.
+
+== Screenshots ==
+
+1. Configuration screen
+2. Activating licensing for a download
+3. Output the generated license keys within your emails
+4. See your generated license keys within the license manager plugin
+
+== Changelog ==
+
+= Version 1.0.0 (17th July 2015) =
+* Initial release
+
+== Upgrade Notice ==
+
+= Version 1.0.0 (17th July 2015) =
+* Initial release
---
+++ edd-software-license-manager.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * Plugin Name:     EDD Software License Manager
+ * Plugin URI:      http://coder.flowdee.de/edd-software-license-manager/
+ * Description:     Seamless integration between Easy Digital Downloads and Software License Manager.
+ * Version:         1.0.0
+ * Author:          flowdee
+ * Author URI:      http://flowdee.de
+ * Text Domain:     edd-slm
+ * Domain Path:     /languages
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 3, as published by the Free Software Foundation. You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @author          flowdee <coder@flowdee.de>
+ * @copyright       Copyright (c) flowdee
+ * @license         http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if( !class_exists( 'EDD_SLM' ) ) {
+
+    /**
+     * Main EDD_SLM class
+     *
+     * @since       1.0.0
+     */
+    class EDD_SLM {
+
+        /**
+         * @var         EDD_SLM $instance The one true EDD_SLM
+         * @since       1.0.0
+         */
+        private static $instance;
+
+
+        /**
+         * Get active instance
+         *
+         * @access      public
+         * @since       1.0.0
+         * @return      object self::$instance The one true EDD_SLM
+         */
+        public static function instance() {
+            if( !self::$instance ) {
+                self::$instance = new EDD_SLM();
+                self::$instance->setup_constants();
+                self::$instance->includes();
+                self::$instance->load_textdomain();
+            }
+
+            return self::$instance;
+        }
+
+
+        /**
+         * Setup plugin constants
+         *
+         * @access      private
+         * @since       1.0.0
+         * @return      void
+         */
+        private function setup_constants() {
+
+            // Plugin version
+            define( 'EDD_SLM_VER', '1.0.0' );
+
+            // Plugin path
+            define( 'EDD_SLM_DIR', plugin_dir_path( __FILE__ ) );
+
+            // Plugin URL
+            define( 'EDD_SLM_URL', plugin_dir_url( __FILE__ ) );
+
+            // SLM Credentials
+            $api_url = rtrim(edd_get_option( 'edd_slm_api_url' ), '/');
+
+            define( 'EDD_SLM_API_URL', $api_url );
+            define( 'EDD_SLM_API_SECRET', edd_get_option( 'edd_slm_api_secret' ) );
+        }
+
+
+        /**
+         * Include necessary files
+         *
+         * @access      private
+         * @since       1.0.0
+         * @return      void
+         */
+        private function includes() {
+
+            // Get out if EDD is not active
+            if( ! function_exists( 'EDD' ) ) {
+                return;
+            }
+
+            // Include files and scripts
+            require_once EDD_SLM_DIR . 'includes/helper.php';
+
+            if ( is_admin() ) {
+                require_once EDD_SLM_DIR . 'includes/meta-boxes.php';
+                require_once EDD_SLM_DIR . 'includes/settings.php';
+            }
+
+            require_once EDD_SLM_DIR . 'includes/emails.php';
+            require_once EDD_SLM_DIR . 'includes/purchase.php';
+        }
+
+        /**
+         * Internationalization
+         *
+         * @access      public
+         * @since       1.0.0
+         * @return      void
+         */
+        public function load_textdomain() {
+
+            // Load the default language files
+            load_plugin_textdomain( 'edd-slm', false, 'edd-software-license-manager/languages' );
+        }
+        
+        /*
+         * Activation function fires when the plugin is activated.
+         *
+         * @since  1.0.0
+         * @access public
+         * @return void
+         */
+        public static function activation() {
+            // nothing
+        }
+
+        /*
+         * Uninstall function fires when the plugin is being uninstalled.
+         *
+         * @since  1.0.0
+         * @access public
+         * @return void
+         */
+        public static function uninstall() {
+            // nothing
+        }
+    }
+
+    /**
+     * The main function responsible for returning the one true EDD_SLM
+     * instance to functions everywhere
+     *
+     * @since       1.0.0
+     * @return      \EDD_SLM The one true EDD_SLM
+     */
+    function EDD_SLM_load() {
+
+        return EDD_SLM::instance();
+    }
+
+    /**
+     * The activation & uninstall hooks are called outside of the singleton because WordPress doesn't
+     * register the call from within the class hence, needs to be called outside and the
+     * function also needs to be static.
+     */
+    register_activation_hook( __FILE__, array( 'EDD_SLM', 'activation' ) );
+    register_uninstall_hook( __FILE__, array( 'EDD_SLM', 'uninstall') );
+
+    add_action( 'plugins_loaded', 'EDD_SLM_load' );
+
+} // End if class_exists check
